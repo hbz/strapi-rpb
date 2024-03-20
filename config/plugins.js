3488348f41994b05b6060ca2540b84928ba66a26
Fabian Steeg <steeg@hbz-nrw.de>
2024-03-20 13:23:49 +0100

    Disable `content-versioning` plugin (RPB-149)

--- config/plugins.js
+++ config/plugins.js
@@ -1,7 +1,7 @@
 // https://github.com/notum-cz/strapi-plugin-content-versioning#%EF%B8%8F-read-before-installation
 module.exports = ({ env }) => ({
 	"content-versioning": {
-		enabled: true,
+		enabled: false,
 	},
 	'lookup': {
 		enabled: true,
