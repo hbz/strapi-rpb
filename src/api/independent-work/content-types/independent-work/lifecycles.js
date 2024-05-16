27b130e09e27d78e5e0b350dda40e22496834615
Fabian Steeg <steeg@hbz-nrw.de>
2024-05-16 13:42:55 +0200

    Set up `person` lookup and label, extract `labelHelper` (RPB-164)

--- src/api/independent-work/content-types/independent-work/lifecycles.js
+++ src/api/independent-work/content-types/independent-work/lifecycles.js
@@ -1,12 +1,7 @@
 module.exports = {
-    beforeCreate(event) {
-        const { data } = event.params;
-        const { v4: uuidv4 } = require("uuid");
-        data.rppdId = data.rppdId || uuidv4();
-    },
     async afterFindOne(event) {
         const helper = require('../../../labelHelper');
-        const lookupFields = ["gndSubjectCategory", "placeOfActivity", "professionOrOccupation", "publication", "relatedPerson", "source"];
+        const lookupFields = ["person"];
         const { result } = event;
         for (const field of lookupFields.filter((f) => result && result.hasOwnProperty(f))) {
             for (const component of result[field]) {
