e1ee08522a7ed37dc963ab22f48cb3639116fb06
Fabian Steeg <steeg@hbz-nrw.de>
2024-05-28 13:23:01 +0200

    Enable `de` locale in app config, remove others (RPB-165)
    Admin UI language can be set in Profile > Interface Language

--- src/admin/app.js
+++ src/admin/app.js
@@ -3,34 +3,7 @@ import { Typography } from '@strapi/design-system';
 import { useCMEditViewDataManager } from "@strapi/helper-plugin";
 
 const config = {
-  locales: [
-    // 'ar',
-    // 'fr',
-    // 'cs',
-    // 'de',
-    // 'dk',
-    // 'es',
-    // 'he',
-    // 'id',
-    // 'it',
-    // 'ja',
-    // 'ko',
-    // 'ms',
-    // 'nl',
-    // 'no',
-    // 'pl',
-    // 'pt-BR',
-    // 'pt',
-    // 'ru',
-    // 'sk',
-    // 'sv',
-    // 'th',
-    // 'tr',
-    // 'uk',
-    // 'vi',
-    // 'zh-Hans',
-    // 'zh',
-  ],
+  locales: ['de'],
 };
 
 const bootstrap = (app) => {
