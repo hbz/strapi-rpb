9f225adf0ef90d842bf16e59afdc46255f75858e
Fabian Steeg <steeg@hbz-nrw.de>
2024-07-01 14:35:01 +0200

    Include model.singularName (content type) in backup file name (#39)
    Resulting in `timestamp_type_id.json` files

--- src/api/backupHelper.js
+++ src/api/backupHelper.js
@@ -1,6 +1,6 @@
 module.exports = {
     saveToDisk: (event) => {
-        const filename = `backup/${event.result.updatedAt}_${event.result.id}.json`;
+        const filename = `backup/${event.result.updatedAt}_${event.model.singularName}_${event.result.id}.json`;
         const content = JSON.stringify({ data: event.result });
         require("fs").writeFile(filename, content, function (error) {
             console.log(error ? error : "Saved to: " + filename);
