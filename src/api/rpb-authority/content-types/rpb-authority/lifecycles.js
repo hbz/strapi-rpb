aa8ebfbbe7a58ea71ea39f5ebf045c0527457016
Fabian Steeg <steeg@hbz-nrw.de>
2024-07-05 16:28:09 +0200

    Fix label call in rpb-authorities lifecycle hook (RPB-175)

--- src/api/rpb-authority/content-types/rpb-authority/lifecycles.js
+++ src/api/rpb-authority/content-types/rpb-authority/lifecycles.js
@@ -5,7 +5,7 @@ module.exports = {
         const { result } = event;
         for (const field of lookupFields.filter((f) => result && result.hasOwnProperty(f))) {
             for (const component of helper.componentsFor(field, result)) {
-                component.label = await helper.labelFor(component.value);
+                component.label = await helper.labelFor(component);
                 component.label = component.label && helper.trimmed(component.label);
             }
         }
