a13a97e899e50e67716910cfd602dc141125ccf7
Fabian Steeg <steeg@hbz-nrw.de>
2024-09-09 14:24:32 +0200

    Set relatedEntity labels in afterFindMany hook, make label public
    To display relatedEntity labels in rpb-authorities lookup (RPB-187)

--- src/api/rpb-authority/content-types/rpb-authority/lifecycles.js
+++ src/api/rpb-authority/content-types/rpb-authority/lifecycles.js
@@ -1,3 +1,16 @@
+const setLabels = async (results) => {
+    const helper = require('../../../labelHelper');
+    const lookupFields = ["relatedEntity"];
+    for (const result of results) {
+        for (const field of lookupFields.filter((f) => result && result.hasOwnProperty(f))) {
+            for (const component of helper.componentsFor(field, result)) {
+                component.label = await helper.labelFor(component);
+                component.label = component.label && helper.trimmed(component.label);
+            }
+        }
+    }
+}
+
 module.exports = {
     beforeCreate(event) {
         const { data } = event.params;
@@ -5,14 +18,9 @@ module.exports = {
         data.rpbId = data.rpbId || uuidv4();
     },
     async afterFindOne(event) {
-        const helper = require('../../../labelHelper');
-        const lookupFields = ["relatedEntity"];
-        const { result } = event;
-        for (const field of lookupFields.filter((f) => result && result.hasOwnProperty(f))) {
-            for (const component of helper.componentsFor(field, result)) {
-                component.label = await helper.labelFor(component);
-                component.label = component.label && helper.trimmed(component.label);
-            }
-        }
+        await setLabels([event.result]);
+    },
+    async afterFindMany(event) {
+        await setLabels(event.result);
     },
 };
