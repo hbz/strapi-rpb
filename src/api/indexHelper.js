cf24d97d950e212611e841dd7289475f5874f404
Fabian Steeg <steeg@hbz-nrw.de>
2024-12-11 13:55:08 +0100

    Send indexing PUT to BiblioVino as well for un-caching
    For global caching in BiblioVino, it needs to know about updates
    (un-cache indexed entry to allow seeing the update in the UI)

--- src/api/indexHelper.js
+++ src/api/indexHelper.js
@@ -3,13 +3,25 @@ const fetch = require("node-fetch");
 module.exports = {
     index: async (event) => {
         const secret = strapi.config.get('admin.auth.secret', '');
-        const url = `http://test.rpb.lobid.org/${event.result.rpbId}?secret=${secret}`; // http://host.docker.internal:9000
-        const response = await fetch(url, {
-            method: 'PUT',
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify(event.result),
-        });
-        console.log(`Indexing response for ${event.result.rpbId} to ${url}:`,
-            response.status, response.statusText, await response.text());
+        for(const url of urls(event.result.inCollection)) {
+            const response = await fetch(`${url}/${event.result.rpbId}?secret=${secret}`, {
+                method: 'PUT',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify(event.result),
+            });
+            console.log(`Indexing response for ${event.result.rpbId} to ${url}:`,
+                response.status, response.statusText, await response.text());
+        }
     }
+}
+
+const urls = (inCollection) => {
+    const result = [];
+    if(inCollection.includes("RPB")) {
+        result.push("http://test.rpb.lobid.org"); // http://host.docker.internal:9000
+    }
+    if(inCollection.includes("BiblioVino")) {
+        result.push("http://test.wein.lobid.org"); // http://host.docker.internal:9000
+    }
+    return result;
 }
